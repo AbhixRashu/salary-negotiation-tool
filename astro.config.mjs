0a420742f513b8a00cde18121584a7a7523d39c9
Rashu <b2rgaming66@gmail.com>
2026-07-27 10:25:47 +0530

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel({ maxDuration: 30 }),
-    vite: {
-        plugins: [tailwindcss()],
-    },
-});
+  site: 'https://salarypitcher.com',
+  output: 'server',
+  adapter: vercel({ maxDuration: 30 }),
 
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
+});
